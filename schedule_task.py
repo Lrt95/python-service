cd2cdf8e0e17169b6dad500d69f09087e9fba210
Antony Correia <antony.correia@grtgaz.com>
2021-03-30 12:12:52 +0200

    refacto

--- schedule_task.py
+++
@@ -1,9 +0,0 @@
-""" Module schedule_task
-
-Created by Antony Correia
-Python Docstring
-"""
-
-from apscheduler.schedulers.background import BackgroundScheduler
-
-scheduler = BackgroundScheduler()
