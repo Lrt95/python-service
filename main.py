8cd716695df8e78e3da89deb9b2e8850a37a307a
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2021-03-30 14:25:42 +0200

    parsing line command finish
    parsing line command finish

--- main.py
+++ main.py
@@ -11,20 +11,28 @@ def usage_line_command(message="Usage : python3 main.py [*.yml or *.yaml]"):
 
 
 def parsing_arg(argv):
+    list_config_file = []
     if len(argv) == 2:
         if ((argv[1].find(".yml") == len(argv[1]) - 4) | (argv[1].find(".yaml") == len(argv[1]) - 5)):
-            parser = argparse.ArgumentParser()
-            parser.add_argument('file')
-            args = parser.parse_args()
-            print(args)
+            if len(argv[1].split(".")) == 2:
+                parser = argparse.ArgumentParser()
+                parser.add_argument('file', type=argparse.FileType('r'))
+                args = parser.parse_args()
+                for line in args.file.readlines():
+                    list_config_file.append(line[:len(line) - 1]);
+            else:
+                usage_line_command()
         else:
             usage_line_command()
     else:
         usage_line_command()
+    return  list_config_file
 
 
 def main():
-    parsing_arg(sys.argv)
+    list_config_file = parsing_arg(sys.argv)
+    if len(list_config_file) > 0 :
+        print(list_config_file)
 
 
 if __name__ == '__main__':
