bf275d366aed61c56d1d3119f8dbf4563b18e61c
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2021-03-31 09:28:59 +0200

    in function consumer
    in function consumer

--- consumer.py
+++ consumer.py
@@ -2,21 +2,24 @@ import pika
 import time
 import json
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-
-channel = connection.channel()
-
-channel.queue_declare(queue='logs', durable=True)
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.basic_qos(prefetch_count=1)
-
-
 def callback(ch, method, properties, body):
     print(json.loads(body))
 
 
-channel.basic_consume('logs', callback, auto_ack=True)
+def sender():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 
-#  ch.basic_ack(delivery_tag=method.delivery_tag)
+    channel = connection.channel()
 
-channel.start_consuming()
+    channel.queue_declare(queue='logs', durable=True)
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.basic_qos(prefetch_count=1)
+
+    channel.basic_consume('logs', callback, auto_ack=True)
+
+    #  ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    sender()
