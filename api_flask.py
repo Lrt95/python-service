24f9ced309c569ab214365775cd8beafc67b31f1
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2021-03-31 19:40:20 +0200

    update docstring
    update docstring

--- api_flask.py
+++ api_flask.py
@@ -10,6 +10,10 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
+    """Function home generate the response with all information and utils time parameter in route
+
+    :return: json all data
+    """
     if 'time' in flask.request.args:
         time = int(flask.request.args['time'])
     else:
@@ -19,6 +23,10 @@ def home():
 
 @app.route('/idAgent', methods=['GET'])
 def agent():
+    """Function agent generate the response with all information of agent and utils time and id agent parameter in route
+
+    :return: json all data's agent
+    """
     if 'id' and 'time' in flask.request.args:
         id = int(flask.request.args['id'])
         time = int(flask.request.args['time'])
@@ -30,6 +38,11 @@ def agent():
 
 @app.route('/idAgent/hardware', methods=['GET'])
 def agent_hardware():
+    """Function agent_hardware generate the response with hardware information of agent
+    and utils time and id agent and hardware parameter in route
+
+    :return: json hardware data's agent
+    """
     if 'id' and 'time' and 'hardware' in flask.request.args:
         id = int(flask.request.args['id'])
         time = int(flask.request.args['time'])
@@ -41,11 +54,17 @@ def agent_hardware():
 
 
 def api_flask():
+    """Function api_flask is the function principal api
+
+    """
     app.config["DEBUG"] = True
     app.run()
 
 
 def main():
+    """Function main
+
+    """
     api_flask()
 
 
